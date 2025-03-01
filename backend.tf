dbc8aadd2ee500a935a92bfdd88f753efe434e56
muri <murtalabello@gmail.com>
2025-03-01 15:34:46 -0600

    refactor Azure AI infrastructure workflow to simplify Terraform actions and update backend provider configuration

--- backend.tf
+++ backend.tf
@@ -1,8 +1,12 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name   = "terraform-backend-rg"
-    storage_account_name  = "terraformbackendai"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
