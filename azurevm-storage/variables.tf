830032c8cb911c6230b01967353bb73cd4e618f9
muri <murtalabello@gmail.com>
2025-03-10 12:16:40 -0500

    add Azure infrastructure deployment workflow and Terraform configuration

---
+++ azurevm-storage/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_count" {
+  default = 2
+}
+
+variable "admin_username" {
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  default = "SecurePass123!"
+}
+
